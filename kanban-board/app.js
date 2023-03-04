30faa0204a11100a064d1a84f41ef39da63f1fb3
Jaynil Gaglani <g.jaynil2401@gmail.com>
2023-03-04 17:52:38 +0530

    feat: added kanban html, styles

---
+++ kanban-board/app.js
@@ -0,0 +1,20 @@
+/*
+
+const tasks = [
+  {
+    id:1,
+    items:[
+      { id:'', content:'', status:'not_started'}
+    ]
+  },
+  {
+    id:2,
+    items:[],
+  },
+  {
+    id:3,    
+    items:[]
+  }
+]
+}
+*/
