529e5ca12c008f252d62af33c11e359cdb2be386
Jaynil Gaglani <g.jaynil2401@gmail.com>
2023-06-17 17:57:31 +0530

    feat: added variable & lazy loading lists example

--- react-performance/src/VirtualizedList/List.jsx
+++ react-performance/src/VirtualizedList/List.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import items from "./mock.json";
-import Row from "./Row";
-import { FixedSizeList } from "react-window";
+import Row, { GridCell } from "./Row";
+import { FixedSizeList, VariableSizeGrid } from "react-window";
+import VirtualizedListWithInfiniteScroll from "../InfiniteVirtualizedList/VirtualizedListWithInfiniteScroll";
 
 const RowComponent = ({ index, style }) => (
   <Row image={items[index]} number={index} style={style} />
 );
 
-const List = () => {
+const getVariableWidthOrHeight = (index) => {
+  return index % 2 === 0 ? 150 : 100;
+};
+
+export const FixedSizeListExample = () => {
   return (
     <FixedSizeList
       height={500}
@@ -18,6 +23,27 @@ const List = () => {
       {RowComponent}
     </FixedSizeList>
   );
+};
+
+export const VariableSizeGridExample = () => {
+  return (
+    <VariableSizeGrid
+      columnCount={1000}
+      rowCount={1000}
+      height={600}
+      width={600}
+      columnWidth={getVariableWidthOrHeight}
+      rowHeight={getVariableWidthOrHeight}
+    >
+      {GridCell}
+    </VariableSizeGrid>
+  );
+};
+
+export const List = () => {
+  // return <FixedSizeListExample />;
+  // return <VariableSizeGridExample />;
+  return <VirtualizedListWithInfiniteScroll />;
 };
 
 export default List;
--- react-performance/src/VirtualizedList/Row.jsx
+++ react-performance/src/VirtualizedList/Row.jsx
@@ -10,4 +10,23 @@ const Row = ({ image, number, style }) => {
   );
 };
 
+export const GridCell = ({ columnIndex, rowIndex, style }) => {
+  return (
+    <div
+      style={style}
+      className={
+        columnIndex % 2
+          ? rowIndex % 2 === 0
+            ? "GridItemOdd"
+            : "GridItemEven"
+          : rowIndex % 2
+          ? "GridItemOdd"
+          : "GridItemEven"
+      }
+    >
+      Item {rowIndex}, {columnIndex}
+    </div>
+  );
+};
+
 export default Row;
