e059f8c6fcebc9dd3ab61024dda871afb2bbb241
Jaynil Gaglani <g.jaynil2401@gmail.com>
2025-02-04 13:50:22 +0530

    feat: added question description

--- polling-widget/index.js
+++ polling-widget/index.js
@@ -1,5 +1,14 @@
 /**
  * Asked in uber screening round
+ * Design a polling widget where there are 4 options & on click of any option the inner height changes in 
+ * percentages based on the frequency of click of each option
+ * Initially all options are at 0%. 
+ * Additionally, add a throttling loader to restrict multiple button clicks
+ * 
+ * e.g. 1st click on option 1 -> 100%, 0%, 0%, 0%
+ *      2nd click on option 2 -> 50%, 50%, 0%, 0%
+ *      3nd click on option 3 -> 33%, 33%, 33%, 0%
+ *      4th click on option 4 -> 25%, 25%, 25%, 25%
  */
 const optionFrequency = { option1: 0, option2: 0, option3: 0, option4: 0 };
 
